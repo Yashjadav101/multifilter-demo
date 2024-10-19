9b92b7014e2e55efa60b2e3eedf8e75d6c65ab40
Yash Jadav <yashjadav650@gmail.com>
2024-10-19 19:42:01 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Data } from "./Utils/data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [initialData, setInitialData] = useState(Data);
+  const [filterLabels, setFilterLabels] = useState([]);
+
+  useEffect(() => {
+    if (Data?.length) {
+      setFilterLabels(() => {
+        const label = new Set([]);
+        Data?.forEach((element) => {
+          Object.keys(element)?.forEach((item) => {
+            if (item !== "id") {
+              label.add(item);
+            }
+          });
+        });
+        return [...label];
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (filterLabels?.length) {
+      let labelValues = {};
+      filterLabels?.forEach((ele) => {
+        console.log("ele", ele);
+        Data.forEach((element) => {
+          console.log("first", element[ele]);
+          if (element[ele])
+            labelValues[ele] = [...(labelValues[ele] || []), element[ele]];
+        });
+      });
+      console.log("labelValues", labelValues);
+    }
+  }, [filterLabels]);
+
+  console.log("initialData", initialData, filterLabels);
+  return <div className="App"></div>;
+};
 
 export default App;
