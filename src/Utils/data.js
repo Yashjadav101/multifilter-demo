b08a4546acc1ede7fb77ef87874a4b60e9e773b7
Yash Jadav <yashjadav650@gmail.com>
2024-10-19 18:50:16 +0530

    Initial commit

---
+++ src/Utils/data.js
@@ -0,0 +1,158 @@
+export const Data = [
+    {
+      id: 1,
+      name: "foo",
+      city: "dallas",
+      category: "one",
+      subCategory: "one",
+      type: "A",
+      active: "FALSE",
+      lastname: "ssss"
+    },
+    {
+      id: 2,
+      name: "bar",
+      city: "dallas",
+      category: "one",
+      type: "B",
+      subCategory: "one",
+      active: "FALSE"
+    },
+    {
+      id: 3,
+      name: "jim",
+      city: "san francisco",
+      category: "one",
+      subCategory: "one",
+      type: "B",
+      active: "TRUE"
+    },
+    {
+      id: 4,
+      name: "jane",
+      city: "denver",
+      category: "two",
+      subCategory: "two",
+      type: "C",
+      active: "FALSE"
+    },
+    {
+      id: 5,
+      name: "someshwari",
+      city: "surat",
+      category: "three",
+      subCategory: "three",
+      type: "D",
+      active: "TRUE"
+    }
+  ];
+  
+  // export const Data = [
+  //   {
+  //     id: 1,
+  //     title: "men's T-shirt",
+  //     price: 109.95,
+  //     category: "men's clothing",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "men's T-shirt",
+  //     price: 22.3,
+  //     category: "men's clothing",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "men's T-shirt",
+  //     price: 55.99,
+  //     category: "men's clothing",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "T-shirt",
+  //     price: 15.99,
+  //     category: "men's clothing",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "John Hardy Women's Legends",
+  //     price: 695,
+  //     category: "jewelery",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Solid Gold Petite Micropave ",
+  //     price: 168,
+  //     category: "jewelery",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "White Gold Plated Princess",
+  //     price: 9.99,
+  //     category: "jewelery",
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "gold chain",
+  //     price: 10.99,
+  
+  //     category: "jewelery",
+  //   },
+  //   {
+  //     id: 9,
+  //     title: "Laptop",
+  //     price: 64,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 10,
+  //     title: "AirBuds",
+  //     price: 109,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 11,
+  //     title: "Iron",
+  //     price: 109,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 12,
+  //     title: "Laptop",
+  //     price: 114,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 13,
+  //     title: "Laptop",
+  //     price: 599,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 14,
+  //     title: "Iron",
+  //     price: 999.99,
+  //     category: "electronics",
+  //   },
+  //   {
+  //     id: 15,
+  //     title: "Women Top",
+  //     price: 56.99,
+  
+  //     category: "women's clothing",
+  //   },
+  //   {
+  //     id: 16,
+  //     title: "Dress",
+  //     price: 29.95,
+  
+  //     category: "women's clothing",
+  //   },
+  //   {
+  //     id: 17,
+  //     title: "Women Top",
+  //     price: 39.99,
+  
+  //     category: "women's clothing",
+  //   }
+  // ];
+  
